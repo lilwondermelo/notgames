cd3691b22de8381f1ea7afa08e24a9b2f44f80e1
lilwondermelo <lilwondermelo@yandex.ru>
2024-07-26 22:33:19 +0700

    123

--- js/main.js
+++ js/main.js
@@ -2,53 +2,61 @@ import Game from './Model/Game.js';
 let game = null;
 
 $('body').on('mousedown', ".block", function(e){
+    e.stopPropagation();
     game.select(this, e);
 });
 
 $('body').on('mouseup', "*", function(){
+    e.stopPropagation();
     game.cancel();
 });
 
 $('body').on('mousemove', "*", function(e){
+    e.stopPropagation();
     game.move(e);
 });
 $('body').on('mousedown', ".block", function(e){
+    e.stopPropagation();
     game.select(this, e);
 });
 
 $('body').on('mouseup', "*", function(){
+    e.stopPropagation();
     game.cancel();
 });
 
 $('body').on('mousemove', "*", function(e){
+    e.stopPropagation();
     game.move(e);
 });
 
 $('body').on("touchend", "*", function(){
+    e.stopPropagation();
     game.cancel();
 });
 
 $('body').on("touchcancel", "*", function(){
+    e.stopPropagation();
     game.cancel();
 });
 $('body').on("touchmove", "*", function(e){
+    e.stopPropagation();
     game.move(e.originalEvent.touches[0]);
 });
 
 $('body').on('touchstart', ".block", function(e){
+    e.stopPropagation();
     game.select(this, e.originalEvent.touches[0]);
 });
 
 $('body').on('click', ".start", function(e){
+    e.stopPropagation();
     if (game != null) {
         game.stop_timer();
     }
     game = new Game(6, 6);
     game.start_timer();
 });
-console.log(window);
-console.log(window.Telegram);
-console.log(window.Telegram.WebApp);
 window.Telegram.WebApp.disableVerticalSwipes();
 
 
@@ -57,7 +65,7 @@ function preventCollapse(event) {
     if (window.scrollY === 0) {
       window.scrollTo(0, 1);
     }
-  }
+  } 
   
   // Attach the above function to the touchstart event handler of the scrollable element
   const scrollableElement = document.querySelector('body');
